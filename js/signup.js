313a89b87f3d85432af9ec6c041be82b24c61c6c
mohamedmek <mohamed_abdelkarim@protonmail.com>
2022-12-24 23:30:23 +0200

    validation of signup

--- js/signup.js
+++ js/signup.js
@@ -1,28 +1,46 @@
-// REGEX for first and last name : no numbers or special characters only english letters
+// REGEX for first.last name , city and country : no numbers or special characters only english letters
 let nameRe = /^([a-zA-Z]){2,}$/
 //REGEX for username consisting only of non-whitespaces and at least 2 characters
 let usernameRe = /^\S{2,}$/;
 // email validation REGEX
 let emailRe =   /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/;
+//REGEX for password :  Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character
+let passRe = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/ ; 
 
-let reTest="s@s.co";
-console.log(emailRe.test(reTest))
-document.forms[0].onmouseenter = function(e){
+// let reTest="sS3aaqq$";
+// console.log(passRe.test(reTest))
+document.forms[0].onsubmit = function(e){
     //get inputs
     let firstNameInput = document.querySelector('[name="firstName"]').value;
     let lastNameInput = document.querySelector('[name="lastName"]').value;
     let usernameInput = document.querySelector('[name="username"]').value;
+    let emailInput = document.querySelector('[name="email"]').value;
+    let passInput = document.querySelector('[name="pass"]').value;
+    let repassInput = document.querySelector('[name="repass"]').value;
+    let cityInput = document.querySelector('[name="city"]').value;
+    let countryInput = document.querySelector('[name="country"]').value;
     
     //valid var's
     let fnameValid = nameRe.test(firstNameInput);
     let lnameValid = nameRe.test(lastNameInput);
-    let usernameValid = nameRe.test(usernameInput);
+    let usernameValid = usernameRe.test(usernameInput);
+    let emailValid = emailRe.test(emailInput);
+    let passValid = passRe.test(passInput);
+    let repassValid = passRe.test(repassInput);
+    let cityValid = nameRe.test(cityInput);
+    let countryValid = nameRe.test(countryInput);
+
+
 
     //test validations
-    console.log(fnameValid);
-    console.log(lnameValid);
+    // console.log(fnameValid);
+    // console.log(lnameValid);
+    // console.log(usernameValid);
+    // console.log(emailValid);
+    // console.log(passValid);
+    // console.log(repassValid);
 
-    if(fnameValid===false || lnameValid===false || usernameValid===false){
+    if(fnameValid===false || lnameValid===false || usernameValid===false || emailValid===false || passValid===false || repassValid===false || cityValid===false  || countryValid===false || passInput != repassInput ){
         e.preventDefault();
         console.log("faild")
     }
