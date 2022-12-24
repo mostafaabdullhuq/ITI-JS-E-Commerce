3ec6b3ce50e6b03e3141887a1e589fdf167eedc0
Mostafa Abdullhuq <mostafaabdullhuq@gmail.com>
2022-12-24 20:49:37 +0200

    index filter buttons edit css

--- js/index.js
+++ js/index.js
@@ -83,6 +83,8 @@ $(function () {
                     // when any category button is clicked, do this function
                     button.addEventListener("click", function () {
                         // get the category of the clicked button from the attribute
+                        $(this).siblings(".index-categ-button").removeClass("active");
+                        $(this).addClass("active");
                         let category = this.getAttribute("data-category"),
                             products = document.querySelectorAll(".prod");
                         if (category === "all") {
