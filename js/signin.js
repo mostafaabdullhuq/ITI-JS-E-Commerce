f104d45f43edd0599f584bda94eff605c59ccd21
mohamedmek <mohamed_abdelkarim@protonmail.com>
2022-12-26 21:29:14 +0200

    add check login's email and password in local storage

--- js/signin.js
+++ js/signin.js
@@ -4,7 +4,7 @@ let emailRe =   /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(
 let passRe = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/ ; 
 
 
-document.forms[0].onsubmit = function(e){
+document.forms[0].onmouseenter = function(e){
     let emailInput = document.querySelector('[name="email"]').value;
     let passInput = document.querySelector('[name="pass"]').value;
 
@@ -14,6 +14,41 @@ document.forms[0].onsubmit = function(e){
     if(emailValid===false || passValid===false){
         e.preventDefault();
 
+    }else{
+        checkLocalStorage(emailInput,passInput);
+
     }
 }
-console.log("test")
+// ("zekar@mailinator.com","Pa$$w0rd!");
+
+function checkLocalStorage(email,pass) {
+    let data = window.localStorage.getItem("tasks");
+    if (data) {
+        let tasks = JSON.parse(data);
+        for (let index = 0; index < tasks.length; index++) {
+            // const element = array[index];
+            if(tasks[index].emailAddress === email){
+                console.log('email found!');
+                if (tasks[index].passWord === pass) {
+                    console.log('password correct');
+                }else{
+                    console.log('password incorrect');
+                }
+
+            }else{
+                console.log('email incorrect');
+
+            }
+
+            
+        }
+        console.log(tasks);
+    // console.log(typeof(tasks));
+    // users_list = tasks;
+    }
+}
+//   email , pass
+
+//   if email valid > check pass 
+
+      
