98e4c268a62d67e084b7b44a0a769ffcf8a4c2a6
mohamedmek <mohamed_abdelkarim@protonmail.com>
2022-12-28 18:05:28 +0200

    signin index

--- js/script.js
+++ js/script.js
@@ -429,54 +429,54 @@ export var ecommerceUsers = new Users();
 
 //!!!!!!!!! FOR TESTING ONLY PLEASE REMOVE BEFORE PUBLISHING
 
-// check if there's a user logged in
-let user = ecommerceUsers.validateLoginCookies();
+// // check if there's a user logged in
+// let user = ecommerceUsers.validateLoginCookies();
 
-// if user is not logged in
-if (!user) {
-    // create new user object
-    let user = new User("Group", "Two", "group2@iti.gov.eg", "Admin@1234", "Egypt", "Alexandria", "Lorem ipsum 24 Bld 2", "+101203215478");
+// // if user is not logged in
+// if (!user) {
+//     // create new user object
+//     let user = new User("Group", "Two", "group2@iti.gov.eg", "Admin@1234", "Egypt", "Alexandria", "Lorem ipsum 24 Bld 2", "+101203215478");
 
-    // create account from user object
-    ecommerceUsers.createAccount(user);
+//     // create account from user object
+//     ecommerceUsers.createAccount(user);
 
-    // login to user account
-    user = ecommerceUsers.loginAccount(user.emailAddress, user.passWord);
+//     // login to user account
+//     user = ecommerceUsers.loginAccount(user.emailAddress, user.passWord);
 
-    // add items to user cart
-    if (user.cart.prodsCount === 0) {
-        ecommerceUsers.updateCart(user, [
-            {
-                id: 1,
-                title: "lorem ipsum datae alla lorem ipsum datae alla",
-                price: 100,
-                qty: 1,
-                image: "https://images.unsplash.com/photo-1570831739435-6601aa3fa4fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1452&q=80",
-            },
-            {
-                id: 2,
-                title: "Product 2",
-                price: 200,
-                qty: 2,
-                image: "https://images.unsplash.com/photo-1555487505-8603a1a69755?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80",
-            },
-            {
-                id: 3,
-                title: "Product 3",
-                price: 300,
-                qty: 3,
-                image: "https://images.unsplash.com/photo-1580870069867-74c57ee1bb07?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80",
-            },
-            {
-                id: 4,
-                title: "Product 4",
-                price: 400,
-                qty: 4,
-                image: "https://images.unsplash.com/photo-1547949003-9792a18a2601?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
-            },
-        ]);
-    }
-}
+//     // add items to user cart
+//     if (user.cart.prodsCount === 0) {
+//         ecommerceUsers.updateCart(user, [
+//             {
+//                 id: 1,
+//                 title: "lorem ipsum datae alla lorem ipsum datae alla",
+//                 price: 100,
+//                 qty: 1,
+//                 image: "https://images.unsplash.com/photo-1570831739435-6601aa3fa4fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1452&q=80",
+//             },
+//             {
+//                 id: 2,
+//                 title: "Product 2",
+//                 price: 200,
+//                 qty: 2,
+//                 image: "https://images.unsplash.com/photo-1555487505-8603a1a69755?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80",
+//             },
+//             {
+//                 id: 3,
+//                 title: "Product 3",
+//                 price: 300,
+//                 qty: 3,
+//                 image: "https://images.unsplash.com/photo-1580870069867-74c57ee1bb07?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80",
+//             },
+//             {
+//                 id: 4,
+//                 title: "Product 4",
+//                 price: 400,
+//                 qty: 4,
+//                 image: "https://images.unsplash.com/photo-1547949003-9792a18a2601?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+//             },
+//         ]);
+//     }
+// }
 
 //!!!!!!!!! FOR TESTING ONLY PLEASE REMOVE BEFORE PUBLISHING
 
@@ -484,7 +484,7 @@ if (!user) {
 if (!ecommerceUsers.validateLoginCookies()) {
     $(".user-controls-list").html(`
     <li class="border-bottom mb-1 pb-1">
-        <a class="icon1" data-bs-toggle="modal" href="#exampleModalToggle2"> Sign In </a>
+        <a class="icon1" data-bs-toggle="modal" href="#signInModal"> Sign In </a>
     </li>
     <li>
         <a class="icon1" data-bs-toggle="modal" href="#exampleModalToggle3"> Sign up </a>
