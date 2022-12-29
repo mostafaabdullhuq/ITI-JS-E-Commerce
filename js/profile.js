3fc1738737a0abac439369eee3d2987ff7baedcf
mohamedmek <mohamed_abdelkarim@protonmail.com>
2022-12-29 17:33:31 +0200

    fix city and country inputs

--- js/profile.js
+++ js/profile.js
@@ -22,26 +22,42 @@ console.log(user);
     document.querySelector('[name="city"]').value = user.city;
     document.querySelector('[name="country"]').value = user.country;
 
+    
+    let oldPassInput = document.querySelector('[name="oldpass"]').value;
+    let newPassInput = document.querySelector('[name="newpass"]').value;
+    let repassInput = document.querySelector('[name="repass"]').value;
+    
+    //valid var's
+    
+    let oldPassValid = passRe.test(oldPassInput);
+    let newPassValid = passRe.test(newPassInput);
+    let repassValid = passRe.test(repassInput);
+
+    // console.log(cityInput);
+    // console.log(countryInput);
+    // console.log(addressInput);
+
+    // console.log("///////////////////////////////////");
 
 document.getElementById("userInfo-form").onsubmit = function(e){
     // get inputs
     let firstNameInput = document.querySelector('[name="firstName"]').value;
     let lastNameInput = document.querySelector('[name="lastName"]').value;
     let emailInput = document.querySelector('[name="email"]').value;
-    let oldPassInput = document.querySelector('[name="oldpass"]').value;
-    let newPassInput = document.querySelector('[name="newpass"]').value;
-    let repassInput = document.querySelector('[name="repass"]').value;
     let cityInput = document.querySelector('[name="city"]').value;
     let countryInput = document.querySelector('[name="country"]').value;
     let addressInput = document.querySelector('[name="address"]').value;
-    //valid var's
+
     let fnameValid = nameRe.test(firstNameInput);
     let lnameValid = nameRe.test(lastNameInput);
     let emailValid = emailRe.test(emailInput);
-    let oldPassValid = passRe.test(oldPassInput);
-    let newPassValid = passRe.test(newPassInput);
-    let repassValid = passRe.test(repassInput);
-
+    console.log(cityInput);
+    console.log(countryInput);
+    console.log(addressInput);
+    
+    // console.log(emailValid);
+    // console.log(newPassInput);
+    
 
     if(fnameValid===false || lnameValid===false || emailValid===false){
         e.preventDefault();
@@ -50,7 +66,7 @@ document.getElementById("userInfo-form").onsubmit = function(e){
     else{
         console.log("success")
 
-        console.log( updateAccount(emailInput,emailInput,firstNameInput,lastNameInput,addressInput,countryInput,cityInput));
+        console.log( updateAccount(emailInput,emailInput,firstNameInput,lastNameInput,addressInput,cityInput,countryInput));
     }
     
 
@@ -63,19 +79,34 @@ document.getElementById("pass-form").onsubmit = function(){
 
 function updateAccount(email,nEmail,nFname,nLname,nAddress,nCity,nCountry) {
     let data = window.localStorage.getItem("eCommerceUsers");
+    console.log(nCity);
     if (data) {
         let eCommerceUsers = JSON.parse(data);
+
         for (let index = 0; index < eCommerceUsers.length; index++) {
             if(eCommerceUsers[index].emailAddress === email){
-            
+                console.log(eCommerceUsers[index].city);
+                console.log(nCity);
+                
             eCommerceUsers[index].emailAddress = nEmail;
             eCommerceUsers[index].firstName = nFname;
             eCommerceUsers[index].lastName = nLname;
             eCommerceUsers[index].shippingAddr = nAddress;
             eCommerceUsers[index].city = nCity;
             eCommerceUsers[index].country = nCountry;
+            
+
+            // console.log(eCommerceUsers[index].city);
+            // console.log(nCity);
+                
+
+            // eCommerceUsers[index].city = nCity;
+            // eCommerceUsers[index].country = nCountry;
+            
+            console.log(eCommerceUsers);
 
             let a = JSON.stringify(eCommerceUsers);
+            // console.log(a);
             window.localStorage.setItem("eCommerceUsers",a);
             }else{
                 console.log('email incorrect');
@@ -84,7 +115,7 @@ function updateAccount(email,nEmail,nFname,nLname,nAddress,nCity,nCountry) {
 
             
         }
-        console.log(eCommerceUsers);
+        // console.log(eCommerceUsers);
     }
 }
 
